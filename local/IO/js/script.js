1a1f1a74e0ce7c25e72dbb1d06dc6706539ecf7a
ashwath22 <ashwath.anand.22@gmail.com>
2016-09-01 16:09:14 -0700

    final code fro Sean's final project

---
+++ local/IO/js/script.js
@@ -0,0 +1,85 @@
+var socket = io();
+
+      var topics = [
+        { id: 'hammock' },
+        { id: 'couch' },
+        { id: 'mens' },
+        { id: 'croom' },
+      ];
+
+      // set all topics
+      topics.forEach(function(topic) {
+        var id = topic.id;
+        // set DOM element
+        var el = topic.el = document.getElementById(id);
+        // subscribe
+        var channel = topic.channel = 'topic:' + id;
+        socket.on(channel, function(availability){
+          // console.log(channel, availability);
+          // store
+          topic.availability = availability;
+          // show availability into element
+
+          el.innerHTML = id + ': ' + availability;
+        });
+      });
+      console.log(topics);
+
+      // request all topics available
+      socket.emit('request all topics available');
+
+      
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+//    $('.nav').click(function(){
+//       $.fn.fullpage.destroy('all');
+//    });
+
+//    $('.nav').click(function(){
+//      if ( $('#fullpage').css('visibility') == 'visible')
+//        $('#fullpage').css('visibility','hidden');
+//        else $('#fullpage').css('visibility','visible');
+//});
+//    $('.nav').click(function(){
+//      if ( $('#fullpage').css('visibility') == 'visible')
+//        $('#listpage').css('visibility','hidden');
+//        else $('#listpage').css('visibility','visible');
+//});
+
+//    $('.nav').click(function(){
+//            $('#fullpage').toggle('#fullpage');
+//});
+//    
+//    $('.toiletList').click(function(){
+//            $('#fullpage').toggle('#fullpage');
+//});
+    
+//        $('.nav').click(function(){
+//            $('.listpage').toggle('.showListpage');
+//            console.log("TOP");
+//});
+    
+    $(".nav").click(function () {
+                $(".listpage").animate({
+                    top: "-=100vh"
+                }, 1000);
+                return false;
+            });
+    
+    $(document).ready(function(){
+        $('.splashPage').delay(3200).queue(function (next) { 
+            $('.splashPage').css('display', 'none'); 
+            next(); 
+  });
+
+        if (availability = 0) {
+            $('.nav').css('display', 'none');
+        }
+});
+    
+    
+});
+
+
